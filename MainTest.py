1146ba19a639071a06a3ea88c4f0f42470e21b41
gerem66 <geremy.lecaplain66@gmail.com>
2020-11-03 23:45:45 +0100

    First commit

--- MainTest.py
+++ MainTest.py
@@ -6,6 +6,8 @@ from ADBLib import SmartPhone as SP
 # Tu initialise ta variable avec comme seul argument le chemin vers ton ADB
 myPhone = SP(r"C:\Users\Geremindows\Downloads\platform-tools")
 
+
+
 # Pour effectuer une pression :
 #myPhone.Press(500, 500)
 
