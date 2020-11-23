c9cdb3eac9b1c34af2325784e170af009b900cb8
Gerem66 <geremy.lecaplain66@gmail.com>
2020-11-23 12:33:35 +0100

    README example Takescreenshot update

--- ADBLib/ADBLibTest.py
+++ ADBLib/ADBLibTest.py
@@ -11,11 +11,4 @@ myPhone = SP()
 #myPhone.WriteText("heyyy !")
 #cv2.imwrite("test.jpg", myPhone.TakeScreenshot()) # Save screenshot
 
-# Take and show screenshot
-#img = myPhone.TakeScreenshot()                                          # Take screenshot
-#img = cv2.resize(img, (int(len(img[0]) / 4), int(len(img) / 4)))        # Resize : resolution / 4
-#cv2.imshow("test", img)                                                 # Show popup with screenshot
-#cv2.waitKey(0)                                                          # Wait to press key
-#cv2.destroyAllWindows()                                                 # Destroy window
-
 myPhone.Destroy()
