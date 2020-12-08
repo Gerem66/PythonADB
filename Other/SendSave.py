10b82325710ab4370df6c7a75c0425f7fe2e90be
gerem66 <geremy.lecaplain66@gmail.com>
2020-12-08 21:25:02 +0100

    Add test adb files

--- Other/SendSave.py
+++ Other/SendSave.py
@@ -12,3 +12,10 @@ def SendMove(self):
     self.ADB("push recorded_touch_events.txt /data/local/tmp/", quiet=True)
     self.ADB("shell /data/local/tmp/sendevent-arm64 {} /data/local/tmp/recorded_touch_events.txt".format(self.EVENTSCREEN), quiet=True)
     self.ADB("shell rm /data/local/tmp/recorded_touch_events.txt", quiet=True)
+
+def SaveMove():
+    print("[!] Ctrl-C to end recording")
+    os.system("adb exec-out getevent -t /dev/input/event2 > recorded_touch_events.txt")
+    exit(0)
+
+SaveMove()
